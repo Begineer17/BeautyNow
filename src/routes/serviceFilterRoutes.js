b2177417bcdb34faad44ab2fe20822bd3ec0d133
Begineer17 <hunggia200484@yahoo.com>
2025-07-23 17:46:32 +0700

    Update

--- src/routes/serviceFilterRoutes.js
+++ src/routes/serviceFilterRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Op } = require('sequelize');
+const { Op, literal } = require('sequelize');
 const Service = require('../models/Service');
 const Salon = require('../models/Salon');
 const SalonProfile = require('../models/SalonProfile');
